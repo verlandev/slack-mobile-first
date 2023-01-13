55329c929f380ecf08e7eb0b9c0f36e79a82e98f
Bemart93 <bemart93@gmail.com>
2023-01-13 17:42:13 +0100

    ajustar versión mobile

--- index.js
+++ index.js
@@ -1,74 +1,3 @@
-// window.addEventListener('resize', () =>{
-//     console.log(window.innerWidth)
-// })
-
-// // JS en el nav bar
-
-// const button = document.querySelector('.navbar__button')
-// const nav = document.querySelector('.nav__elements')
-// const navbar = document.querySelector('.navbar')
-// const slack = document.querySelector('h5')
-// const svg = document.querySelector('.navbar__button--svg')
-
-
-
-// button.addEventListener('click', () =>{
-    
-//     if (nav.classList.toggle('activo')){
-
-//             navbar.style.background = "#fff";
-//             button.style.background = "#fff";
-//             slack.style.color = "#000";
-//             svg.style.filter="invert(100%)"
-//     } 
-//         else {
-            
-//             navbar.style.background = "#531554";
-//             button.style.background = "#531554";
-//             slack.style.color = "#fff";
-//             svg.style.filter="invert(0%)"    
-//     }   
-// })
-
-// // JS para el carrusel
-
-// const caption = document.querySelector('.addFeatures__caption')
-// const span = document.querySelectorAll('.addFeatures__carrusel--span')
-
-// span.forEach((cadaSpan, i) =>{ 
-//     span[i].addEventListener('click', () =>{
-
-//         let position = i
-//         let operacion = position * -170
-
-//         caption.style.transform = `translateX(${operacion}%)`
-
-//         span.forEach( (cadaSpan, i) => {
-//             span[i].classList.remove('span__active')
-//         })
-//         span[i].classList.add('span__active')
-
-//     })
-
-// })
-
-
-// //JS para el acordeón
-
-// const block     = document.querySelectorAll('.footerSupMenu__block')
-// const h5        = document.querySelectorAll('.footerSupMenu__h5')
-
-// h5.forEach(( eachH5, i) =>{
-//     h5[i].addEventListener('click', accordeon = ()=>{
-
-//         block.forEach((eachBlock, i)=>{
-//             block[i].classList.remove('block--active')
-//         })
-//             block[i].classList.add('block--active')
-//     } )
-
-//     eachH5.removeEventListener
-// })
 
 const block     = document.querySelectorAll('.footerSupMenu__block')
 const h5        = document.querySelectorAll('.footerSupMenu__h5')
@@ -89,11 +18,11 @@ window.addEventListener('resize', () =>{
 
 // JS en el nav bar
 
-const button = document.querySelector('.navbar__button')
-const nav = document.querySelector('.nav__elements')
-const navbar = document.querySelector('.navbar')
-const slack = document.querySelector('h5')
-const svg = document.querySelector('.navbar__button--svg')
+const button       = document.querySelector('.navbar__button')
+const nav          = document.querySelector('.nav__elements')
+const navbar       = document.querySelector('.navbar')
+const slack        = document.querySelector('h5')
+const svg          = document.querySelector('.navbar__button--svg')
 
 
 
